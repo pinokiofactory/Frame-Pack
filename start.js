0617f7d1b54604af634fb0cc7f8c12434ed68dc2
SUP3RMASS1VE <Martincull@live.com>
2025-05-17 23:32:09 +0100

    Update start.js

--- start.js
+++ start.js
@@ -1,5 +1,9 @@
 module.exports = async (kernel) => {
   const port = await kernel.port()
+  
+  console.log("Starting standard version directly")
+  const scriptToRun = "demo_gradio.py"
+  
   return {
     daemon: true,
     run: [
@@ -10,29 +14,20 @@ module.exports = async (kernel) => {
           env: { },                   // Edit this to customize environment variables (see documentation)
           path: "app",                // Edit this to customize the path to start the shell from
           message: [
-            `python demo_gradio.py --server 127.0.0.1 --port ${port}`,    // Edit with your custom commands
+            `python ${scriptToRun} --server 127.0.0.1 --port ${port}`,    // Edit with your custom commands
           ],
           on: [{
-            // The regular expression pattern to monitor.
-            // When this pattern occurs in the shell terminal, the shell will return,
-            // and the script will go onto the next step.
-            "event": "/http:\/\/\\S+/",   
-
-            // "done": true will move to the next step while keeping the shell alive.
-            // "kill": true will move to the next step after killing the shell.
+            "event": "/http:\/\/[0-9.:]+/",    // Regular expression for the expected event
             "done": true
           }]
         }
       },
       {
-        // This step sets the local variable 'url'.
-        // This local variable will be used in pinokio.js to display the "Open WebUI" tab when the value is set.
         method: "local.set",
         params: {
-          // the input.event is the regular expression match object from the previous step
           url: "{{input.event[0]}}"
         }
       }
     ]
-  }
-}
+  };
+};
