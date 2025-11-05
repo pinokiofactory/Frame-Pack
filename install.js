62194bffcc44f703bf7e66b4a3b734d20b52edb9
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-11-05 02:11:22 +0100

    remove comments

--- install.js
+++ install.js
@@ -1,12 +1,9 @@
 module.exports = {
   run: [
-    // Edit this step to customize the git repository to use
     {
       method: "shell.run",
       params: {
         message: [
-          //"git clone https://github.com/SUP3RMASS1VE/FramePack-fork app",
-          //"git clone https://github.com/cocktailpeanut/FramePack app",
           "git clone https://github.com/lllyasviel/FramePack app",
         ]
       }
@@ -25,26 +22,24 @@ module.exports = {
         message: "cp -f demo_gradio_k.py app"
       }
     },
-    // Delete this step if your project does not use torch
     {
       method: "script.start",
       params: {
         uri: "torch.js",
         params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
-          // triton: true   // uncomment this line if your project requires triton
-          // sageattention: true   // uncomment this line if your project requires sageattention
+          venv: "env",
+          path: "app",
+          // xformers: true,
+          // triton: true,
+          // sageattention: true
         }
       }
     },
-    // Edit this step with your custom install commands
     {
       method: "shell.run",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
+        venv: "env",
+        path: "app",
         message: [
           "uv pip install gradio devicetorch",
           "uv pip install -r requirements.txt"
